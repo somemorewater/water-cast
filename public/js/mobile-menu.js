39f9d96efe22db90a9a8011e3e3b70023873db5f
somemorewater <oboyiloejeh@gmail.com>
2026-02-04 20:59:36 +0100

    add mobile menu toggle functionality

---
+++ public/js/mobile-menu.js
@@ -0,0 +1,6 @@
+const mobileMenuBtn = document.getElementById("mobileMenuBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+
+mobileMenuBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
